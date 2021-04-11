be7bfd51815ab8f61aea0f7402a33637528f6257
Audrey Lee <alee2@olin.edu>
2021-04-11 15:52:51 -0400

    finished problem 3

--- BAB/bab_starter.py
+++ BAB/bab_starter.py
@@ -6,7 +6,7 @@ import heapq as hq
 import numpy as np
 import itertools
 import math
-counter = itertools.count() 
+counter = itertools.count()
 
 class BBTreeNode():
     def __init__(self, vars = [], constraints = [], objective='', prob=None):
@@ -22,15 +22,15 @@ class BBTreeNode():
         '''
         newprob = pic.Problem.clone(self.prob)
         return BBTreeNode(self.vars, newprob.constraints, self.objective, newprob)
-    
+
     def buildProblem(self):
         '''
         Bulids the initial Picos problem
         '''
         prob=pic.Problem()
-   
-        prob.add_list_of_constraints(self.constraints)    
-        
+
+        prob.add_list_of_constraints(self.constraints)
+
         prob.set_objective('max', self.objective)
         self.prob = prob
         return self.prob
@@ -61,7 +61,6 @@ class BBTreeNode():
         n2.prob.add_constraint( branch_var >= math.ceil(branch_var.value) ) # add in the new binary constraint
         return n2
 
-
     def bbsolve(self):
         '''
         Use the branch and bound method to solve an integer program
@@ -79,8 +78,38 @@ class BBTreeNode():
         bestres = -1e20 # a small arbitrary initial best objective value
         bestnode_vars = root.vars # initialize bestnode_vars to the root vars
 
-        #TODO: fill this part in
+        # print(heap)
+        while len(heap) > 0:
+            print("Heap Size: ", len(heap))
+            _, _, node = hq.heappop(heap)
+            try:
+                node.prob.solve(solver='cvxopt')
+            except:
+                #if problem is infeasible, don't do that branch
+                print("Infeasible")
+                pass
 
-        
+            print("node_vars: ",  [float(v.value) for v in node.vars])
+            #if the relaxed problem is bad
+            if float(node.vars[-1]) < float(bestres - 1e-2):
+                print("Get rid of branch")
+                continue
+            #if a valid solution then this is the new best
+            elif node.is_integral():
+                print("New Best Integral solution.")
+                bestres = float(node.vars[-1])
+                bestnode_vars = [float(v.value) for v in node.vars]
+            #otherwise, create new branches and branch into it
+            else:
+                for v in node.vars:
+                    # grab the first var that isn't an integer
+                    if abs(round(v.value) - float(v.value)) > 1e-2:
+                        # create the new branches
+                        new_left_node = node.branch_floor(v)
+                        new_right_node = node.branch_ceil(v)
+
+                        # add the branches to the heap
+                        hq.heappush(heap, (float(node.vars[-1]), next(counter), new_left_node))
+                        hq.heappush(heap, (float(node.vars[-1]), next(counter), new_right_node))
+                        break
         return bestres, bestnode_vars
- 
